001ef7a83c95fe0811dc2d2e09bb6a40ce75ba94
Camila <alvescamila87@gmail.com>
2024-04-16 18:43:06 -0300

    create specific queries

--- spring_projeto2_magazine/src/main/java/org/senai/lab365/magazinesenai/repositories/ProdutoRepository.java
+++ spring_projeto2_magazine/src/main/java/org/senai/lab365/magazinesenai/repositories/ProdutoRepository.java
@@ -1,9 +1,18 @@
 package org.senai.lab365.magazinesenai.repositories;
 
 import org.senai.lab365.magazinesenai.models.Produto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProdutoRepository extends CrudRepository<Produto, Long> {
+
+    @Query(value = "SELECT * FROM produtos WHERE p.descricao = :descricao")
+    public Produto findByDescricao(String descricao);
+
+    // Não precisa de @Query (derived queries)
+    public List<Produto> findByNome(String nome);
 }
