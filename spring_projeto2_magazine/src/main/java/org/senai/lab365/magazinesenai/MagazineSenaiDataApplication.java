fc2eee29f1ba2340ba8c329cb60ba15aa252edf6
Camila <alvescamila87@gmail.com>
2024-04-16 15:34:51 -0300

    create project 2: spring data, maven (pom), resources (url), app.properties, create models, create repository

---
+++ spring_projeto2_magazine/src/main/java/org/senai/lab365/magazinesenai/MagazineSenaiDataApplication.java
@@ -0,0 +1,13 @@
+package org.senai.lab365.magazinesenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineSenaiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineSenaiDataApplication.class, args);
+	}
+
+}
