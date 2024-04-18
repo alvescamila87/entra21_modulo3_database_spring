c882e29ceaaa371c5bd9c279d69cb797a2524a71
Camila <alvescamila87@gmail.com>
2024-04-18 16:21:06 -0300

    includes dependencies to generate doc from swagger

--- spring_projeto3_swagger/src/main/java/br/com/labs365/livros/config/SwaggerConfiguration.java
+++ spring_projeto3_swagger/src/main/java/br/com/labs365/livros/config/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.labs365.livros.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -9,28 +10,28 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SwaggerConfig {
+@EnableSwagger2
+public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(getApiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**"))
-                .build()
-                .useDefaultResponseMessages(false);
+                 .build()
+                 .useDefaultResponseMessages(false);
     }
-
     private ApiInfo getApiInfo() {
-        return new ApiInfoBuilder()
-                .title("Livros API")
-                .description("API de Demonstração")
-                .contact(new Contact("John Snow", "www.livros.com", "john@livros.com"))
-                .version("1.0")
-                .build();
-    }
-
+                 return new ApiInfoBuilder()
+                 .title("Livros API")
+                 .description("API de Demonstração")
+                 .contact(new Contact("John Snow", "www.livros.com", "john@livros.com"))
+                 .version("1.0")
+                 .build();
+                 }
 }
